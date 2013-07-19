5efd5eeeeb2d9ec4545dd42d5a7c6059fcf37d04
Adam Bender <bendera@google.com>
2013-07-18 18:12:53 -0700

    adding p22

--- lib/ninetyninedarts.dart
+++ lib/ninetyninedarts.dart
@@ -44,6 +44,8 @@ removeAt(int n, List l){}
 
 insertAt(Object e, int n, List l){}
 
+range(int start, int end){}
+
 bool iterableEquals(Iterable l1, Iterable l2){
   if(l1.length != l2.length){
     return false;
